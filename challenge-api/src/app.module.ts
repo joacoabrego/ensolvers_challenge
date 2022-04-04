0db173d892808fda52c964b7ed5d3908fb2e9fb4
Joaquin Abrego <joaquin_abrego96@hotmail.com>
2022-04-03 21:40:50 -0300

    Bash script update

--- challenge-api/src/app.module.ts
+++ challenge-api/src/app.module.ts
@@ -3,17 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { Todo } from './typeorm/Todo';
 
 @Module({
   imports: [
     TodosModule,
+    ConfigModule.forRoot({
+      envFilePath: '../.env',
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
       port: 3306,
-      username: 'joaquin',
-      password: 'root',
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
       database: 'todos_db',
       entities: [Todo],
       synchronize: true,
--- run.sh
+++ run.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-    echo "Enter your MySQL username:"
-    read username
-    echo "Enter your MySQL password:"
-    read password
-    echo "Checking if MySQL service is running.."
-    apt install sysvinit-utils
-    service mysql start
+    source .env
     echo "Starting MySQL server..."
-    mysql --user="$user" --password="$password"
+    mysql --user="$DATABASE_USER" --password="$DATABASE_PASSWORD" --execute="CREATE DATABASE todos_db"
     cd challenge-api
-    npm run start:dev
+    npm run start:dev &
+    cd ../challenge-frontend
+    npm start
