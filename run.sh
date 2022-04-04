3d9bc3de098dfb66be5455938f7204516ceda1ea
Joaquin Abrego <joaquin_abrego96@hotmail.com>
2022-04-03 22:08:16 -0300

    Updated bash script

--- run.sh
+++ run.sh
@@ -3,6 +3,12 @@
     echo "Starting MySQL server..."
     mysql --user="$DATABASE_USER" --password="$DATABASE_PASSWORD" --execute="CREATE DATABASE todos_db"
     cd challenge-api
+    npm install &
+    process_id=$!
+    wait $process_id
     npm run start:dev &
+    process_id=$!
     cd ../challenge-frontend
+    npm install &
+    wait
     npm start
